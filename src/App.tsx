2c38f6621650a409304ca93820b082908b7a6261
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2023-02-07 11:32:20 +0100

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import CardCombatBoard from './components/BattleBord/cardCombatBoard';
-import GridBattleBoard from './components/BattleBord/gridBattleBoard';
 import NewTournament from './components/NewTournament/newTournament';
+import Home from './components/Home';
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <NewTournament />
-        </div>
+        <Routes>
+            <Route path="/">
+                <Route index element={<Home />}></Route>
+                <Route path="/tournament" element={<NewTournament />} />
+            </Route>
+        </Routes>
     );
 }
 
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <>
+            <p>main page</p>
+        </>
+    );
+}
+
+export default Home;
