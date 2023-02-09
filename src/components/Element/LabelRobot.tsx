19875a7378c2c119e8e89be7adc0f8cb094391fe
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2023-02-09 17:41:51 +0100

    update label

---
+++ src/components/Element/LabelRobot.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Button, Row } from 'antd';
+import { blue, yellow } from '@ant-design/colors';
+import Label from './Label';
+import { RobotInterface } from '../../api/RobotApi';
+import { BattleInterface } from '../../api/BattleApi';
+import { CrownFilled } from '@ant-design/icons';
+function isWinner(fighter: RobotInterface, battle: BattleInterface) {
+    return battle.win === undefined ? battle.win : battle.win?._id === fighter._id;
+}
+
+export enum WinPos {
+    START = 'start',
+    END = 'end',
+    TWING = 'twing',
+}
+interface LabelRobotProps {
+    fighter: RobotInterface;
+    pos: WinPos;
+    battle: BattleInterface;
+    color: string;
+}
+const LabelRobot: React.FC<React.PropsWithChildren<LabelRobotProps>> = (props) => {
+    const { fighter, battle, color, pos, children } = props;
+
+    const win = isWinner(fighter, battle);
+
+    const style: React.CSSProperties = { backgroundColor: color };
+
+    if (win === true) {
+        style.borderWidth = '2px';
+        style.borderStyle = 'solid';
+        style.borderColor = 'green';
+    } else if (win === false) {
+        style.borderWidth = '2px';
+        style.border = '2px';
+        style.borderStyle = 'solid';
+        style.borderColor = 'red';
+    }
+
+    const winElment = win ? (
+        fighter._id === battle.win?._id ? (
+            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5 }} />
+        ) : (
+            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />
+        )
+    ) : (
+        <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />
+    );
+
+    return (
+        <Row>
+            {pos === WinPos.START
+                ? winElment
+                : pos === WinPos.TWING && (
+                      <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />
+                  )}
+            <Label color={blue[5]} win={win}>
+                {children}
+            </Label>
+            {(pos === WinPos.END || pos === WinPos.TWING) && winElment}
+        </Row>
+    );
+};
+export default LabelRobot;
--- src/components/Fight/FightBoard.tsx
+++ src/components/Fight/FightBoard.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Card, Divider, Col, Row, Typography } from 'antd';
+import { Card, Divider, Row, Typography } from 'antd';
 import { BattleInterface, getBattles } from '../../api/BattleApi';
 import { RobotInterface } from '../../api/RobotApi';
-import { blue, yellow } from '@ant-design/colors';
-import Label from '../Element/Label';
-import { CrownFilled } from '@ant-design/icons';
+import { blue } from '@ant-design/colors';
+import LabelRobot, { WinPos } from '../Element/LabelRobot';
 
 function isWinner(fighter: RobotInterface, battle: BattleInterface) {
     return battle.win === undefined ? battle.win : battle.win?._id === fighter._id;
@@ -17,54 +16,82 @@ interface BattleLineProps {
 export const BattleLine: React.FC<BattleLineProps> = (props) => {
     const { battle } = props;
 
-    return battle.fighters.length >= 2 ? (
-        <Row gutter={[18, 18]} style={{ textAlign: 'center', verticalAlign: 'middle' }}>
-            <Col span={8}>
-                <Row gutter={[18, 18]} justify={'start'}>
-                    <Label color={blue[5]} win={isWinner(battle.fighters[0], battle)}>
-                        <Typography.Text>{battle.fighters[0].name}</Typography.Text>
-                    </Label>
-                    {battle.win ? (
-                        battle.fighters[0]._id === battle.win._id ? (
-                            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5 }} />
-                        ) : (
-                            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />
-                        )
-                    ) : (
-                        <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />
-                    )}
-                </Row>
-            </Col>
-            <Col span={2}>
+    const labels = battle.fighters.map((fighter, index, array) => {
+        // console.log(fighter);
+        return (
+            <Row key={index}>
+                <LabelRobot
+                    color={blue[5]}
+                    fighter={fighter}
+                    battle={battle}
+                    pos={index == 0 ? WinPos.END : array.length == index + 1 ? WinPos.START : WinPos.TWING}
+                >
+                    <Typography.Text>{fighter.name}</Typography.Text>
+                </LabelRobot>
+            </Row>
+        );
+    });
+
+    const lineElement: JSX.Element[] = [];
+
+    labels.forEach((element, index, array) => {
+        lineElement.push(element);
+        if (array.length !== index + 1)
+            lineElement.push(
                 <Typography.Text strong type={'warning'} style={{ whiteSpace: 'nowrap' }}>
                     vs
                 </Typography.Text>
-            </Col>
-            <Col span={8}>
-                <Row gutter={[18, 18]} justify={'end'}>
-                    {battle.win ? (
-                        battle.fighters[1]._id === battle.win._id ? (
-                            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5 }} />
-                        ) : (
-                            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />
-                        )
-                    ) : (
-                        <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />
-                    )}
-                    <Label color={yellow[5]} win={isWinner(battle.fighters[1], battle)}>
-                        <Typography.Text>{battle.fighters[1].name}</Typography.Text>
-                    </Label>
-                </Row>
-            </Col>
-            <Col span={2}>
-                <Divider type={'vertical'} style={{ height: '100%' }} />
-            </Col>
-            <Col span={4}>
-                <Typography.Text style={{ whiteSpace: 'nowrap' }}>{battle.status}</Typography.Text>
-            </Col>
+            );
+    });
+
+    return (
+        <Row gutter={[18, 18]} style={{ textAlign: 'center', verticalAlign: 'middle' }} justify={'space-between'}>
+            {lineElement.map((value) => value)}
+
+            {/*<Col span={8}>*/}
+            {/*    <Row gutter={[18, 18]} justify={'start'}>*/}
+            {/*        <Label color={blue[5]} win={isWinner(battle.fighters[0], battle)}>*/}
+            {/*            <Typography.Text>{battle.fighters[0].name}</Typography.Text>*/}
+            {/*        </Label>*/}
+            {/*        {battle.win ? (*/}
+            {/*            battle.fighters[0]._id === battle.win._id ? (*/}
+            {/*                <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5 }} />*/}
+            {/*            ) : (*/}
+            {/*                <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />*/}
+            {/*            )*/}
+            {/*        ) : (*/}
+            {/*            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />*/}
+            {/*        )}*/}
+            {/*    </Row>*/}
+            {/*</Col>*/}
+            {/*<Col span={2}>*/}
+            {/*    <Typography.Text strong type={'warning'} style={{ whiteSpace: 'nowrap' }}>*/}
+            {/*        vs*/}
+            {/*    </Typography.Text>*/}
+            {/*</Col>*/}
+            {/*<Col span={8}>*/}
+            {/*    <Row gutter={[18, 18]} justify={'end'}>*/}
+            {/*        {battle.win ? (*/}
+            {/*            battle.fighters[1]._id === battle.win._id ? (*/}
+            {/*                <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5 }} />*/}
+            {/*            ) : (*/}
+            {/*                <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />*/}
+            {/*            )*/}
+            {/*        ) : (*/}
+            {/*            <CrownFilled style={{ color: yellow[5], fontSize: '20px', padding: 5, opacity: 0 }} />*/}
+            {/*        )}*/}
+            {/*        <Label color={yellow[5]} win={isWinner(battle.fighters[1], battle)}>*/}
+            {/*            <Typography.Text>{battle.fighters[1].name}</Typography.Text>*/}
+            {/*        </Label>*/}
+            {/*    </Row>*/}
+            {/*</Col>*/}
+            {/*<Col span={2}>*/}
+            {/*    <Divider type={'vertical'} style={{ height: '100%' }} />*/}
+            {/*</Col>*/}
+            {/*<Col span={4}>*/}
+            {/*    <Typography.Text style={{ whiteSpace: 'nowrap' }}>{battle.status}</Typography.Text>*/}
+            {/*</Col>*/}
         </Row>
-    ) : (
-        <></>
     );
 };
 
@@ -73,8 +100,8 @@ const FightBoard: React.FC = () => {
 
     React.useEffect(() => {
         const interval = setInterval(() => {
-            getBattles().then((battle) => {
-                setBattles(battle);
+            getBattles().then((battles) => {
+                setBattles(battles.filter((element) => element?.fighters));
             });
         }, 1000);
         return () => clearInterval(interval);
